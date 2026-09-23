e123666f708ff3363cbcf6252231958fddec244d
Ian-S <ianshyu2008@gmail.com>
2026-09-23 19:17:46 +0000

    fdas

--- Weight.java
+++ Weight.java
@@ -3,11 +3,7 @@ public class Weight {
     private int ounces;
 
     public Weight(int ounces){
-        if (ounces >= 0){
-            if (ounces < 16){
-                this.ounces = ounces;
-            }
-        }
+       this(ounces,0);
  
     }
 
@@ -21,5 +17,33 @@ public class Weight {
     }
     if (pounds > 0){
         this.pounds = pounds;
+    }
+    
+    public int totalOunces(){
+        return this.ounces + 16 * this.pounds;
+    }
+    
+    public boolean (isHeavier(Weight other)){
+        int totalW = this.totalOunces();
+        int otherW = other.totalOunces;
+
+        if (totalW > otherW)
+            return true;
+        else
+            return false;
+    
+    public Weight multiple(int scale){
+        int x = scale * this.totalOunces();
+        int newP = x/16;
+        int newO = total - (p&16)
+        return new Weight(newO,newP)  
+    }
+
+    public void print(){
+        System.out.println(this.pounds + " pounds," + this.ounces + " ounces")
+    }
+
+    
+
     }
 }
