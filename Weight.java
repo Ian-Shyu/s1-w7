79d6b00123eb9059ae13ac855f284dab1627c82d
Ian-Shyu <ianshyu2008@gmail.com>
2026-09-23 18:23:07 +0000

    fdsaf

---
+++ Weight.java
@@ -0,0 +1,24 @@
+public class Weight {
+    private int pounds;
+    private int ounces;
+
+    public Weight(int ounces){
+        if (ounces >= 0){
+            if (ounces < 16){
+                this.ounces = ounces;
+            }
+        }
+ 
+    }
+
+    public Weight(int ounces, int pounds){
+       if (ounces >= 0){
+            if (ounces < 16){
+                this.ounces = ounces;
+            }
+        }
+ 
+    }
+    if (pounds > 0) 
+    }
+}
